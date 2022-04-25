825bebb800c00c74c8c6c28d182264682b6ba3ed
Igor <spuraigor@gmail.com>
2022-04-25 20:54:59 +0300

    Factorial and Just Coding

--- JavaBasic/src/JustCodingForEnjoy/Inkaps.java
+++ JavaBasic/src/JustCodingForEnjoy/Inkaps.java
@@ -4,5 +4,13 @@ public class Inkaps {
     public static void main(String[] args) {
         Outer outer = new Outer();
         Outer.displayInnerContext();
+        Lessons_15 lessons_15 = new Lessons_15();
+        lessons_15.myMethod();
+        lessons_15.displayMesagge(new Message() {
+            @Override
+            public String sayHello() {
+                return "Привет";
+            }
+        });
     }
 }
