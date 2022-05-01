9588bc957dfcc07cdf72a32a94d53886a0a21eaf
Igor <spuraigor@gmail.com>
2022-05-01 16:25:56 +0300

    HomeWork_5

--- JavaBasic/src/HomeWork_5/Test.java
+++ JavaBasic/src/HomeWork_5/Test.java
@@ -6,9 +6,31 @@ public class Test {
         System.out.println(Math.PI);
         double x = 28;
         double y = 4;
+        double f = 26.76;
 
         // Возвращает максимум из 2 чисел
         System.out.println("максимум из 2 чисел: " +Math.max(x, y));
+
+        // Возвращает минимум из 2 чисел
+        System.out.println("минимум из 2 чисел: " +Math.min(x, y));
+
+       //Используется для округления десятичных чисел до ближайшего значения.
+        System.out.println("Округление числа  f : " + Math.round(f));
+
+        //Используется для поиска наименьшего целочисленного значения, которое больше или равно аргументу или математическому целому числу
+        System.out.println("Округление числа к большему  f : " + Math.ceil(f));
+
+        //Используется для поиска наименьшего целочисленного значения, которое меньше или равно аргументу или математическому целому числу
+        System.out.println("Округление числа к меньшему  f : " + Math.floor(f));
+
+       //рандом из 0 до 1
+        System.out.println("Рандом от 1 до 1 : " + Math.random());
+
+        //рандом из 0 до 100 c помощью метода округления
+        System.out.println("Рандом от 1 до 100 : " + Math.round(Math.random()* 100));
+
+        //Используется для округления десятичных чисел до ближайшего значения.
+        System.out.println("Округление числа  f : " + Math.round(f));
 
         // возвращает квадратный корень
         System.out.println("квадратный корень : " + Math.sqrt(y));
@@ -30,40 +52,10 @@ public class Test {
         // вернуть степень 2
         System.out.println("exp of a is: " +Math.exp(x));
 
-        // return (a power of 2)-1
-        System.out.println("expm1 of a is: " +Math.expm1(x));
+        //вертнет абсолютное значение заданого значения
+        System.out.println("абсолютное значение заданого значения : " + Math.abs(x));
 
-        double a = 30;
 
-        // converting values to radian
-        double b = Math.toRadians(a);
-
-        // return the trigonometric sine of a
-        System.out.println("Sine value of a is: " +Math.sin(a));
-
-        // return the trigonometric cosine value of a
-        System.out.println("Cosine value of a is: " +Math.cos(a));
-
-        // return the trigonometric tangent value of a
-        System.out.println("Tangent value of a is: " +Math.tan(a));
-
-        // return the trigonometric arc sine of a
-        System.out.println("Sine value of a is: " +Math.asin(a));
-
-        // return the trigonometric arc cosine value of a
-        System.out.println("Cosine value of a is: " +Math.acos(a));
-
-        // return the trigonometric arc tangent value of a
-        System.out.println("Tangent value of a is: " +Math.atan(a));
-
-        // return the hyperbolic sine of a
-        System.out.println("Sine value of a is: " +Math.sinh(a));
-
-        // return the hyperbolic cosine value of a
-        System.out.println("Cosine value of a is: " +Math.cosh(a));
-
-        // return the hyperbolic tangent value of a
-        System.out.println("Tangent value of a is: " +Math.tanh(a));
 
 
 
